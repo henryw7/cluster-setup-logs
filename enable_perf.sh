4b6929cd8c7f65819be169b9f296fb45168283d2
Henry Wang <henrywang0142857@gmail.com>
2025-02-21 13:37:24 -0800

    Enable perf profiling

---
+++ enable_perf.sh
@@ -0,0 +1,13 @@
+### Software
+# OS: Ubuntu 24.04.1 LTS
+
+### Problem
+# I want to run the following perf commands in user mode
+perf record -F 9999 -o perf.data -g python pyscf_reference.py
+perf report --stdio -n -g
+
+### Solution
+# Permanently turn it on
+sudo vim /etc/sysctl.conf # Add kernel.perf_event_paranoid=0
+# Temporarily turn it on
+sudo sysctl kernel.perf_event_paranoid=0
